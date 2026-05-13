d7c559b5c479f448e8a4c0df4170c798000d38c6
Jason Lau <i@dotkrnl.com>
2026-05-12 22:26:17 -0700

    style: remove unused import in logic_retry_rules test

--- tests/logic/logic_retry_rules.rs
+++ tests/logic/logic_retry_rules.rs
@@ -1,5 +1,5 @@
 use codexize::logic::pipeline::Phase;
-use codexize::logic::rules::{RetryTarget, retry_phase_for_stage, retry_target_for_run, stage_str};
+use codexize::logic::rules::{RetryTarget, retry_phase_for_stage, retry_target_for_run};
 use codexize::state::{RunRecord, RunStatus};
 
 #[test]
